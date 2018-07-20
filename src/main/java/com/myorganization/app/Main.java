bfd983557b82e7f0305084112b43cb4d5a0d25e3
tim t <tim.telf+githubFEep@gmail.com>
2018-07-20 14:43:03 +0300

    Update Main.java

--- src/main/java/com/myorganization/app/Main.java
+++ src/main/java/com/myorganization/app/Main.java
@@ -3,6 +3,6 @@ package com.myorganization.app;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world! My own");
+		System.out.println("Hello, world! My own - my next");
 	}
 }
